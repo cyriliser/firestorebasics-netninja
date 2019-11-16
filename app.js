26b3c1d77a09e2b363b147ca8d3b6a5ac5a512d6
siyathokoza <siyathokozacmbatha@gmail.com>
2019-11-17 01:08:55 +0200

    added functionality to delete data into firestore

--- app.js
+++ app.js
@@ -6,15 +6,28 @@ const rendercafe = doc => {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
+  cross.textContent = "x";
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafelist.appendChild(li);
+
+  // deleting data
+  cross.addEventListener("click", e => {
+    e.stopPropagation(); //stops the event from bubbling up
+    let id = e.target.parentElement.getAttribute("data-id");
+    // console.log(id);
+    db.collection("cafes")
+      .doc(id)
+      .delete();
+  });
 };
 
 // getting data
