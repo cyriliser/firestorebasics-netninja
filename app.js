2eab43fee7facd7915a42ad57d426cbb86ca12a1
siyathokoza <siyathokozacmbatha@gmail.com>
2019-11-17 00:44:43 +0200

    connected and got data from firestore

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+const cafelist = document.querySelector("#cafe-list");
+
+// create element and render cafe
+const rendercafe = doc => {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+
+  li.appendChild(name);
+  li.appendChild(city);
+
+  cafelist.appendChild(li);
+};
+
 db.collection("cafes")
   .get()
   .then(snapshot => {
     // console.log(snapshot.docs);
+
     snapshot.forEach(doc => {
-      console.log(doc.data());
+      // console.log(`Object id: ${doc.id}`);
+      // console.log(doc.data());
+      rendercafe(doc);
     });
   });
